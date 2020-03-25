88f0250402b1210772ada72625a9cdd552b91ee2
Yasin Çalışkan <yaskocaliskan26@gmail.com>
2020-03-25 21:37:04 +0300

    Created Travel Component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TravelComponent } from './travel/travel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TravelComponent
   ],
   imports: [
     BrowserModule,
