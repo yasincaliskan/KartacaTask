9124b4ea22f746247a8fabde0d03a4cae75639e2
Yasin Çalışkan <yaskocaliskan26@gmail.com>
2020-04-04 19:43:00 +0300

    Fixed configuration

--- backend/Kartaca/Kartaca/settings.py
+++ backend/Kartaca/Kartaca/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'post',
 ]
 
@@ -49,6 +50,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:4200",
+    "http://127.0.0.1:8000"
 ]
 
 ROOT_URLCONF = 'Kartaca.urls'
